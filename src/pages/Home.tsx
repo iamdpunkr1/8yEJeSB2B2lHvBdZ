0a6c919c316a3dab0c46e91ca6e714058fcca23c
Dipankar Prasad <iamdpunkr@gmail.com>
2023-12-06 22:23:21 +0530

    fix(landing): Update candidate details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,27 @@
-
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <section>
-        Landing Page
-    </section>
+    <div>
+      <section>
+        <h2>My Details</h2>
+        <p>First Name: Dipankar Prasad</p>
+        <p>Highest Degree Passout Year: July, 2023</p>
+        <p>Technologies/Languages: ReactJS, React-Native, NodeJS, ExpressJS, MongoDB, MySQL, C++, TailwindCSS, Git, Photoshop</p>
+      </section>
+      <section>
+        <h2>Download Resume</h2>
+        <Link to="/dipankar__resume.pdf" download>
+          Download Resume
+        </Link>
+      </section>
+      <section>
+        <h2>BMI Calculation</h2>
+        <p>
+          <Link to="/bmi">Calculate BMI</Link>
+        </p>
+      </section>
+    </div>
   )
 }
 
