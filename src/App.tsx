423099907d542a8fc4e6fe4bd1519861fbf1b1b4
Dipankar Prasad <iamdpunkr@gmail.com>
2023-12-06 22:10:10 +0530

    feat(landing): implement initial landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
-import './App.css'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
 
   return (
     <main>
-      Cubix Craft
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </main>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <section>
+        Landing Page
+    </section>
+  )
+}
+
+export default Home
