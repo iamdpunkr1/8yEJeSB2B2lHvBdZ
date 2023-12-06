3a0aab670f1f0d68b022516e7ff8b3f754f2a743
Dipankar Prasad <iamdpunkr@gmail.com>
2023-12-06 22:31:35 +0530

    style(landing): Add styling for candidate details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,32 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <section>
-        <h2>My Details</h2>
-        <p>First Name: Dipankar Prasad</p>
-        <p>Highest Degree Passout Year: July, 2023</p>
-        <p>Technologies/Languages: ReactJS, React-Native, NodeJS, ExpressJS, MongoDB, MySQL, C++, TailwindCSS, Git, Photoshop</p>
-      </section>
-      <section>
-        <h2>Download Resume</h2>
-        <Link to="/dipankar__resume.pdf" download>
-          Download Resume
-        </Link>
-      </section>
-      <section>
-        <h2>BMI Calculation</h2>
-        <p>
-          <Link to="/bmi">Calculate BMI</Link>
-        </p>
-      </section>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md">
+        <section className="mb-6">
+          <h2 className="text-3xl font-bold mb-4">My Details</h2>
+          <p className="text-gray-700 mb-2">First Name: Dipankar Prasad</p>
+          <p className="text-gray-700 mb-2">Highest Degree Passout Year: July, 2023</p>
+          <p className="text-gray-700">Technologies/Languages: ReactJS, React-Native, NodeJS, ExpressJS, MongoDB, MySQL, C++, TailwindCSS, Git, Photoshop</p>
+        </section>
+        <section className="mb-6">
+          <h2 className="text-3xl font-bold mb-4">Download Resume</h2>
+          <Link to="/dipankar__resume.pdf" download className="text-blue-500 underline hover:text-blue-700">
+            Download Resume
+          </Link>
+        </section>
+        <section>
+          <h2 className="text-3xl font-bold mb-4">BMI Calculation</h2>
+          <p>
+            <Link to="/bmi" className="text-blue-500 underline hover:text-blue-700">
+              Calculate BMI
+            </Link>
+          </p>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
